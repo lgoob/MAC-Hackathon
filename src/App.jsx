2e26d96363d0458ab6fee29299b8b3bf0d3eed5c
saumyaaanand <s3931404@student.rmit.edu.au>
2024-05-03 22:32:33 +1000

    first route to Home

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
     <>
       <div>
         <Router>
+          <Routes>
           <Route path="/" element={<Home />} />
+          </Routes>
         </Router>
       </div>
     </>
