a82b6ddb70fef73ef1d520ee8b0054f30b70b33c
lgoob <s3950137@student.rmit.edu.au>
2024-05-04 20:45:03 +1000

    Gucci lookin Homepage WITHOUT TIMER PLZ PUT IN THANKS

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {     
+      backgroundImage: {
+        'hero': "url('./pictures/Cafe (Night).jpg')",
+      },
+    },
   },
   variants: {},
   plugins: [],
