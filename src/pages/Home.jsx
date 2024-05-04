6557cb7d4c30e2d185d05d64dbbc52cd28f5c071
Mason Cao <s3967383@student.rmit.edu.au>
2024-05-04 16:12:29 +1000

    TailwindCSS now works correctly

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import HeadLineFullPage from "../components/HeadLineFullPage";
 
 function Home() {
-  return <div>Hello!!</div>;
+  return (
+    <div>
+      <HeadLineFullPage
+        title="Welcome to the Whiteboard"
+        secondary="Create and share your own whiteboards."
+      />
+    </div>
+  );
 }
 
 export default Home;
