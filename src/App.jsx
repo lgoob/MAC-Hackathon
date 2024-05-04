81dfc5f60687e9e18505ad959aa7cd226c841b2c
Joy El-Hayek <s3972828@student.rmit.edu.au>
2024-05-04 12:55:12 +1000

    Add images to the sticky notes choices

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Home from "./pages/Home";
 import Whiteboard from "./pages/Whiteboard";
+import StickyNote from "./components/StickyNote";
+import StickyNoteChoice from './components/StickyNoteChoice';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [visiblePopup, setVisiblePopup] = useState(false);
+
+  function getStickyNoteChoice() {
+    setVisiblePopup(true);
+    // addNote();
+  }
+
+  function handleClose() {
+    setVisiblePopup(false);
+  };
+
+  // function addNote() {
+  //     setNotes([
+  //         ...notes,
+  //         {
+  //             id: Date.now(),
+  //         },
+  //     ])
+  // }
+  
+  function removeNote(noteId) {
+      setNotes(notes.filter((item) => item.id !== noteId))
+  }
+
   return (
     <>
       <div>
@@ -14,8 +39,18 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/Whiteboard" element={<Whiteboard />} />
+            <Route path="/StickyNotes" element={<StickyNote />} />
           </Routes>
         </Router>
+      </div>
+      <div className="App">
+          <button className="sticky-btn" onClick={getStickyNoteChoice}>
+              Create Note +
+          </button>
+          {visiblePopup && <StickyNoteChoice isVisible={visiblePopup} onClose={handleClose} />}
+          {/* {notes.map((item) => (
+              <StickyNote key={item.id} onClose={() => removeNote(item.id)} />
+          ))} */}
       </div>
     </>
   );
