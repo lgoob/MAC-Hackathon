11eb2c5e4a85d4cf1d88798412c4a83d098ff8aa
Mason Cao <s3967383@student.rmit.edu.au>
2024-05-04 16:12:29 +1000

    TailwindCSS now works correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import Home from "./pages/Home";
 import Whiteboard from "./pages/Whiteboard";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
