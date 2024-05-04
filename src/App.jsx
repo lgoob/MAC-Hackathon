ded2028cf0ae3cd132b1371b32612d0b5b2fadff
Mason Cao <s3967383@student.rmit.edu.au>
2024-05-04 18:52:06 +1000

    Modernised look of task items

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Whiteboards from "./pages/Whiteboards";
 import Whiteboard from "./pages/Whiteboard";
-import Corkboard from './components/Corkboard'; // Make sure the path is correct
+import Corkboard from "./components/Corkboard"; // Make sure the path is correct
 import StickyNotesPage from "./pages/StickyNotesPage";
-
 
 function App() {
   return (
@@ -12,12 +11,13 @@ function App() {
       <div>
         <Router>
           <Routes>
-            <Route path="/" element={<Home />} />
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/" element={<StickyNotesPage />} />
             <Route path="/Whiteboard" element={<Whiteboard />} />
-            <Route path="/Corkboard" element={<Corkboard />} /> 
+            <Route path="/Corkboard" element={<Corkboard />} />
             <Route path="/whiteboards" element={<Whiteboards />} />
             <Route path="/whiteboards/:id" element={<Whiteboard />} />
-            <Route path="/stickynote" element={<StickyNotesPage />} />
+            {/* <Route path="/stickynote" element={<StickyNotesPage />} /> */}
           </Routes>
         </Router>
       </div>
