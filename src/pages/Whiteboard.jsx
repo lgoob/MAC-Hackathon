b173ecd5df19d93cadc5a25d89b2485af43e8af8
lgoob <s3950137@student.rmit.edu.au>
2024-05-04 14:49:43 +1000

    Can create and name whiteboards

--- src/pages/Whiteboard.jsx
+++ src/pages/Whiteboard.jsx
@@ -1,15 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DrawingCanvas from "../components/DrawingCanvas";
 import Pens from "../components/Utility Bar/Pens";
+import { useParams } from "react-router-dom";
 
 function Whiteboard() {
-  const [color, setColor] = useState("red");
+  const { id } = useParams();
+  const [color, setColor] = useState("black");
+  const [strokes, setStrokes] = useState([]);
+
+  useEffect(() => {
+    // Load strokes from localStorage
+    const savedStrokes = localStorage.getItem(`whiteboard_${id}`);
+    if (savedStrokes) {
+      try {
+        setStrokes(JSON.parse(savedStrokes));
+      } catch (e) {
+        console.error("Error parsing strokes from localStorage", e);
+        setStrokes([]);
+      }
+    } else {
+      setStrokes([]);
+    }
+  }, [id]);
+
+  useEffect(() => {
+    // Save strokes to localStorage when they change, but only after the initial load
+    if (strokes.length > 0) {
+      localStorage.setItem(`whiteboard_${id}`, JSON.stringify(strokes));
+    }
+  }, [strokes, id]);
 
   return (
     <div>
-      <h1>Draw your masterpiece on our whiteboard</h1>
-      <DrawingCanvas color={color}></DrawingCanvas>
-      <Pens setColor={setColor}></Pens> {/* Update setColour to setColor */}
+      <h1>Draw your masterpiece on whiteboard {id}</h1>
+      <DrawingCanvas color={color} strokes={strokes} setStrokes={setStrokes} id={id} />
+      <Pens setColor={setColor} />
     </div>
   );
 }
