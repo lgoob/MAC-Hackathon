559ab79535c6cb865624a02e647c4910870bc425
Joy El-Hayek <s3972828@student.rmit.edu.au>
2024-05-04 17:03:56 +1000

    Fixed import statements

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Whiteboards from "./pages/Whiteboards";
 import Whiteboard from "./pages/Whiteboard";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Corkboard from './components/Corkboard'; // Make sure the path is correct
+import StickyNotesPage from "./pages/StickyNotesPage";
 
 
 function App() {
@@ -17,6 +17,7 @@ function App() {
             <Route path="/Corkboard" element={<Corkboard />} /> 
             <Route path="/whiteboards" element={<Whiteboards />} />
             <Route path="/whiteboards/:id" element={<Whiteboard />} />
+            <Route path="/stickynote" element={<StickyNotesPage />} />
           </Routes>
         </Router>
       </div>
