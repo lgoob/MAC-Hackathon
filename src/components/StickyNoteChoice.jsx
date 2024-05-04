c05ccc369510986c1c59f8e791eace351d888194
Mason Cao <s3967383@student.rmit.edu.au>
2024-05-04 19:29:57 +1000

    Improved modal overlay

--- src/components/StickyNoteChoice.jsx
+++ src/components/StickyNoteChoice.jsx
@@ -4,27 +4,47 @@ import TaskList from "../photosAndIcons/TaskList.png";
 import FreeTextNote from "../photosAndIcons/FreeTextNote.png";
 
 function StickyNoteChoice({ isVisible, onClose, onTaskNote, onFreeTextNote }) {
-    if (!isVisible) return null; // If not visible, don't render the component
+  if (!isVisible) return null; // If not visible, don't render the component
 
-    return (
-        <div className="popup-overlay">
-            <div className="popup-form">
-                <form>
-                    <div>
-                        <p>Please select one of the 2 available options:</p>
-                        <button type="button" onClick={onTaskNote}>
-                            Task List
-                            <img src={TaskList} alt="taskListStickyNote" />
-                        </button>
-                        <button type="button" onClick={onFreeTextNote}>
-                            Free Text
-                            <img src={FreeTextNote} alt="freeTextStickyNote" />
-                        </button>
-                    </div>
-                </form>
+  return (
+    <div className="popup-overlay">
+      <div className="popup-form">
+        <form>
+          <div>
+            <p className="text-2xl font-bold mb-5">Pick your sticky note</p>
+            <div className="flex flex-row space-x-5">
+              <div
+                className="bg-white rounded-lg shadow-md p-4 w-48 border-2"
+                onClick={onTaskNote}
+              >
+                <button type="button" className="w-full">
+                  <p className="text-sm font-semibold mb-3">Task list</p>
+                  <img
+                    src={TaskList}
+                    alt="taskListStickyNote"
+                    className="w-full"
+                  />
+                </button>
+              </div>
+              <div
+                className="bg-white rounded-lg shadow-md p-4 w-48 border-2"
+                onClick={onFreeTextNote}
+              >
+                <button type="button" className="w-full">
+                  <p className="text-sm font-semibold mb-3">Free text</p>
+                  <img
+                    src={FreeTextNote}
+                    alt="freeTextStickyNote"
+                    className="w-full"
+                  />
+                </button>
+              </div>
             </div>
-        </div>
-    );
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default StickyNoteChoice;
