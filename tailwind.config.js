e97933284af5f4494dfda11e0e67f6c41b89283e
Joy El-Hayek <s3972828@student.rmit.edu.au>
2024-05-05 11:17:00 +1000

    added comma to stop code from breaking :)

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       backgroundImage: {
 
         'hero': "url('./pictures/Cafe (Night).jpg')",
-        'whiteboards': "url('./pictures/whiteboardsbackground.png')"
+        'whiteboards': "url('./pictures/whiteboardsbackground.png')",
         whiteboard: "url('./pictures/Whiteboard_Background.jpg')",
 
       },
